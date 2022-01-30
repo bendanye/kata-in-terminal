ccde867556c8da7f5ada82dca4918850323dd73a
benjaminng <shen_soonng@hotmail.com>
2022-01-30 11:48:50 +0800

    added incorrect_ans_file as another kata alternative

--- kata/kata_factory.py
+++ kata/kata_factory.py
@@ -1,8 +1,14 @@
 from kata.kata_from_file import KataFromFile
+from kata.kata_from_incorrect_ans_file import KataFromIncorrectAnsFile
+
+kata_types = {
+    "file": KataFromFile,
+    "incorrect_ans_file": KataFromIncorrectAnsFile
+}
 
 
 class KataFactory():
 
     @staticmethod
     def get_kata(type, folder_path, questions_folder_name, solutions_folder_name):
-        return KataFromFile(folder_path, questions_folder_name, solutions_folder_name)
+        return kata_types[type](folder_path, questions_folder_name, solutions_folder_name)
---
+++ tests/test_kata_factory.py
@@ -0,0 +1,13 @@
+from kata.kata_factory import KataFactory
+
+
+def test_should_get_kata_file():
+    kata = KataFactory.get_kata("file", "path", "questions", "solutions")
+    assert str(type(kata)) == "<class 'kata.kata_from_file.KataFromFile'>"
+
+
+def test_should_get_kata_file():
+    kata = KataFactory.get_kata(
+        "incorrect_ans_file", "path", "questions", "solutions")
+    assert str(type(
+        kata)) == "<class 'kata.kata_from_incorrect_ans_file.KataFromIncorrectAnsFile'>"
